c0720f5c13437a93fd3bf91679fcb7d8cafd3a67
dgahn <dgahn@si-analytics.ai>
2021-04-02 23:45:38 +0900

    회원가입 로직 추가

---
+++ server-launcher/src/main/kotlin/me/dgahn/application/MemberService.kt
@@ -0,0 +1,44 @@
+package me.dgahn.application
+
+import me.dgahn.entity.Member
+import me.dgahn.repo.MemberRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class MemberService(
+    @Autowired
+    val memberRepo: MemberRepository
+) {
+
+    /**
+     * 회원 가입
+     */
+
+    @Transactional
+    fun signUp(member: Member): Long {
+        validateDuplicateMember(member)
+        memberRepo.save(member)
+        return member.id
+    }
+
+    private fun validateDuplicateMember(member: Member) {
+        val findMembers = memberRepo.findByName(member.name)
+        if (findMembers.isNotEmpty()) {
+            throw IllegalStateException("이미 존재하는 회원입니다.")
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+
+    fun findMembers(): List<Member> {
+        return memberRepo.findAll()
+    }
+
+    fun findOne(memberId: Long) = memberRepo.findOne(memberId)
+
+}

851d022453c1f90ae2bbadb62e268255c8f12985
dgahn <dgahn@si-analytics.ai>
2021-04-17 15:17:15 +0900

    공통 인터페이스 적용

--- spring-data-jpa-example/src/main/kotlin/me/dgahn/repository/TeamRepository.kt
+++ spring-data-jpa-example/src/main/kotlin/me/dgahn/repository/TeamRepository.kt
@@ -1,7 +1,7 @@
 package me.dgahn.repository
 
-import me.dgahn.entity.Team
+import me.dgahn.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-interface TeamRepository : JpaRepository<Team, Long>
+interface TeamRepository: JpaRepository<Member, Long>
--- spring-data-jpa-example/src/test/kotlin/me/dgahn/repository/MemberRepositoryTest.kt
+++ spring-data-jpa-example/src/test/kotlin/me/dgahn/repository/MemberRepositoryTest.kt
@@ -2,6 +2,7 @@ package me.dgahn.repository
 
 import io.kotest.core.spec.style.AnnotationSpec
 import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import me.dgahn.entity.Member
 import org.springframework.boot.test.context.SpringBootTest
@@ -23,5 +24,31 @@ open class MemberRepositoryTest(
 
         val findMember = memberRepository.findById(savedMember.id!!).get()
         findMember shouldBe savedMember
+    }
+
+    @Test
+    fun `구성원에 대한 CRUD 테스트`() {
+        val member1 = Member(username = "member1", age = 10)
+        val member2 = Member(username = "member2", age = 12)
+
+        memberRepository.save(member1)
+        memberRepository.save(member2)
+
+        val findMember1 = memberRepository.findById(member1.id!!)
+        val findMember2 = memberRepository.findById(member2.id!!)
+
+        findMember1.get() shouldBe member1
+        findMember2.get() shouldBe member2
+
+        val members = memberRepository.findAll()
+        members shouldHaveSize 2
+
+        val count = memberRepository.count()
+        count shouldBe 2
+
+        memberRepository.delete(member1)
+        memberRepository.delete(member2)
+        val deletedCount = memberRepository.count()
+        deletedCount shouldBe 0
     }
 }
